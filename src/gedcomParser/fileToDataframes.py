c14fbb8d548c728273783c73d5903a74d6256c69
Martin Gruber <martin.gruber1@web.de>
2018-09-30 14:46:26 -0400

    Fix empty dataframe problem

--- src/gedcomParser/fileToDataframes.py
+++ src/gedcomParser/fileToDataframes.py
@@ -16,21 +16,27 @@ def parseFileToDFs(filename: str) -> Tuple[pd.DataFrame, pd.DataFrame]:
     indivs_df = pd.DataFrame(indivs)
     families_df = pd.DataFrame(families)
 
-    # Calculate age
-    indivs_df['AGE'] = [np.nan if birth is np.nan else
-                        relativedelta(parse_date(death) if death is not np.nan else date.today(), parse_date(birth)).years
-                        for birth, death in zip(indivs_df['BIRTHDAY'], indivs_df['DEATH'])]
+    indivs_columns = ['ID', 'NAME', 'GENDER', 'BIRTHDAY', 'AGE', 'ALIVE', 'DEATH', 'CHILD', 'SPOUSE']
+    fams_columns = ['ID', 'MARRIED', 'DIVORCED', 'HUSBAND ID', 'HUSBAND NAME', 'WIFE ID', 'WIFE NAME', 'CHILDREN']
 
-    # Calculate alive
-    indivs_df['ALIVE'] = [birth is not np.nan and death is not np.nan
-                          for birth, death in zip(indivs_df['BIRTHDAY'], indivs_df['DEATH'])]
+    if indivs_df.empty:
+        indivs_df = pd.DataFrame(columns=indivs_columns)
+    else:
+        # Calculate age
+        indivs_df['AGE'] = [np.nan if birth is np.nan else
+                relativedelta(parse_date(death) if death is not np.nan else date.today(), parse_date(birth)).years
+                for birth, death in zip(indivs_df['BIRTHDAY'], indivs_df['DEATH'])]
+        # Calculate alive
+        indivs_df['ALIVE'] = [birth is not np.nan and death is not np.nan
+                for birth, death in zip(indivs_df['BIRTHDAY'], indivs_df['DEATH'])]
+        # Reorder columns
+        indivs_df = indivs_df[indivs_columns]
 
-    # Reorder columns
-    if not indivs_df.empty:
-        indivs_df = indivs_df[['ID', 'NAME', 'GENDER', 'BIRTHDAY', 'AGE', 'ALIVE', 'DEATH', 'CHILD', 'SPOUSE']]
-    if not families_df.empty:
-        families_df = families_df[
-            ['ID', 'MARRIED', 'DIVORCED', 'HUSBAND ID', 'HUSBAND NAME', 'WIFE ID', 'WIFE NAME', 'CHILDREN']]
+    if families_df.empty:
+        families_df = pd.DataFrame(columns=fams_columns)
+    else:
+        # Reorder columns
+        families_df = families_df[fams_columns]
 
     return indivs_df, families_df
 
