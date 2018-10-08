e94292f13513fac2cf097d2fb8ec084d376e39fc
Martin Gruber <martin.gruber1@web.de>
2018-10-08 15:14:03 -0400

    Create tests

---
+++ test/unitTests.py
@@ -0,0 +1,34 @@
+import sys
+
+sys.path.append('..')
+
+from gedcomValidater import validate
+from gedcomValidater.gedcomParser.fileToDataframes import parseFileToDFs
+import unittest
+from unittest import TestCase
+import pytest
+import pandas as pd
+import numpy as np
+
+
+class TestDivorceBeforeDeath(TestCase):
+    def test(self):
+        indivs_df, fams_df = parseFileToDFs("../gedcom_files/test_divorce_before_death.ged")
+        div_after_death = validate.divorce_before_death(indivs_df, fams_df)
+        expected = {'ID': {0: '@shmi@'}, 'NAME': {0: 'Shmi /Skywalker/'}, 'DEATH': {0: '16 MAY 1977'},
+                    'DIVORCED': {0: '10 MAY 1978'}}
+        self.assertEqual(div_after_death[['ID', 'NAME', 'DEATH', 'DIVORCED']].to_dict(), expected)
+
+
+class TestLessThan150yearsOld(TestCase):
+    def test(self):
+        indivs_df, fams_df = parseFileToDFs("../gedcom_files/test_less_than_150_years_old.ged")
+        indivs_150 = validate.less_than_150_years_old(indivs_df)
+        expected = {'ID': {0: '@ani@', 2: '@luke@'}, 'NAME': {0: 'Anakin /Skywalker/', 2: 'Luke /Skywalker/'},
+                    'BIRTHDAY': {0: '25 MAY 1777', 2: '26 SEP 1865'}, 'DEATH': {0: '19 MAY 2005', 2: np.nan},
+                    'AGE': {0: 227.0, 2: 153.0}}
+        self.assertEqual(indivs_150[['ID', 'NAME', 'BIRTHDAY', 'DEATH', 'AGE']].to_dict(), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
